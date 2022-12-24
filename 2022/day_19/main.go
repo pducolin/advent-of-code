9ac9598a6f3c3e049851acec936aae9f3c9a18d3
pducolin <paola.ducolin@datadoghq.com>
2022-12-24 10:06:29 +0100

    [2022] day 19 Skip test part 2, it is too slow

--- 2022/day_19/main.go
+++ 2022/day_19/main.go
@@ -4,6 +4,10 @@ import (
 	_ "embed"
 	"flag"
 	"fmt"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 //go:embed input.txt
@@ -24,9 +28,266 @@ func main() {
 }
 
 func part1(data string) string {
-	return data
+	lines := strings.Split(data, "\n")
+	resultsChannel := make(chan int, len(lines))
+
+	worker := func(line string, result chan int) {
+		bp := NewBlueprint(line)
+		result <- bp.id * bp.FindMaxGeodes(24)
+	}
+
+	for _, line := range lines {
+		go worker(line, resultsChannel)
+	}
+
+	totQualityLevel := 0
+	count := 0
+	for count < len(lines) {
+		res := <-resultsChannel
+		totQualityLevel += res
+		count++
+	}
+	return strconv.Itoa(totQualityLevel)
 }
 
 func part2(data string) string {
-	return data
+	lines := strings.Split(data, "\n")
+	if len(lines) > 3 {
+		lines = lines[0:3]
+	}
+	resultsChannel := make(chan int, len(lines))
+
+	worker := func(line string, result chan int) {
+		bp := NewBlueprint(line)
+		result <- bp.FindMaxGeodes(32)
+	}
+
+	for _, line := range lines {
+		go worker(line, resultsChannel)
+	}
+
+	totQualityLevel := 1
+	count := 0
+	for count < len(lines) {
+		res := <-resultsChannel
+		totQualityLevel *= res
+		count++
+	}
+	return strconv.Itoa(totQualityLevel)
+}
+
+type Material int
+
+const (
+	ore Material = iota
+	clay
+	obsidian
+	geode
+)
+
+var MATERIALS = []Material{ore, clay, obsidian, geode}
+
+type RobotCost struct {
+	material Material
+	cost     map[Material]int
+}
+
+func NewRobotCost(material Material) RobotCost {
+	return RobotCost{
+		material: material,
+		cost:     map[Material]int{},
+	}
+}
+
+type Blueprint struct {
+	id                        int
+	robotCostByTargetMaterial map[Material]RobotCost
+}
+
+const blueprintRegex = `Blueprint (\d+): Each ore robot costs (\d+) ore\. Each clay robot costs (\d+) ore\. Each obsidian robot costs (\d+) ore and (\d+) clay\. Each geode robot costs (\d+) ore and (\d+) obsidian\.`
+
+func NewBlueprint(s string) Blueprint {
+	bp := Blueprint{
+		robotCostByTargetMaterial: map[Material]RobotCost{},
+	}
+	r := regexp.MustCompile(blueprintRegex)
+
+	groups := r.FindStringSubmatch(s)
+
+	id, err := strconv.Atoi(groups[1])
+	if err != nil {
+		panic(err)
+	}
+	bp.id = id
+
+	robotCost := NewRobotCost(ore)
+	robotCost.cost[ore], err = strconv.Atoi(groups[2])
+	if err != nil {
+		panic(err)
+	}
+	bp.robotCostByTargetMaterial[ore] = robotCost
+
+	robotCost = NewRobotCost(clay)
+	robotCost.cost[ore], err = strconv.Atoi(groups[3])
+	if err != nil {
+		panic(err)
+	}
+	bp.robotCostByTargetMaterial[clay] = robotCost
+
+	robotCost = NewRobotCost(obsidian)
+	robotCost.cost[ore], err = strconv.Atoi(groups[4])
+	if err != nil {
+		panic(err)
+	}
+	robotCost.cost[clay], err = strconv.Atoi(groups[5])
+	if err != nil {
+		panic(err)
+	}
+	bp.robotCostByTargetMaterial[obsidian] = robotCost
+
+	robotCost = NewRobotCost(geode)
+	robotCost.cost[ore], err = strconv.Atoi(groups[6])
+	if err != nil {
+		panic(err)
+	}
+	robotCost.cost[obsidian], err = strconv.Atoi(groups[7])
+	if err != nil {
+		panic(err)
+	}
+	bp.robotCostByTargetMaterial[geode] = robotCost
+
+	return bp
+}
+
+func (bp *Blueprint) TotNeedByMaterial(material Material) int {
+	res := 0
+	for _, robotCost := range bp.robotCostByTargetMaterial {
+		res += robotCost.cost[material]
+	}
+	return res
+}
+
+type RobotState struct {
+	time               int
+	robotsByMaterial   map[Material]int
+	resourceByMaterial map[Material]int
+}
+
+func (rs *RobotState) NextState() RobotState {
+	nextState := RobotState{
+		time:               rs.time + 1,
+		robotsByMaterial:   map[Material]int{},
+		resourceByMaterial: map[Material]int{},
+	}
+
+	for k, v := range rs.robotsByMaterial {
+		nextState.robotsByMaterial[k] = v
+	}
+
+	for k, v := range rs.resourceByMaterial {
+		nextState.resourceByMaterial[k] = v + rs.robotsByMaterial[k]
+	}
+
+	return nextState
+}
+
+func (rs *RobotState) HashString() string {
+	hash := strconv.Itoa(rs.time)
+	for _, material := range MATERIALS {
+		hash += fmt.Sprintf("%d:%d", material, rs.resourceByMaterial[material])
+		hash += fmt.Sprintf("%d:%d", material, rs.robotsByMaterial[material])
+	}
+	return hash
+}
+
+func (rs *RobotState) Score() int {
+	return ((rs.robotsByMaterial[geode]+rs.resourceByMaterial[geode])*10+
+		(rs.robotsByMaterial[obsidian]+rs.resourceByMaterial[obsidian]))*10 +
+		(rs.robotsByMaterial[clay]+rs.resourceByMaterial[clay])*10 +
+		rs.robotsByMaterial[ore] + rs.resourceByMaterial[ore]
+}
+
+func (bp *Blueprint) CanBuild(status RobotState, targetMaterial Material) bool {
+	if targetMaterial != geode && status.robotsByMaterial[targetMaterial] >= bp.TotNeedByMaterial(targetMaterial) {
+		return false
+	}
+	robotCost := bp.robotCostByTargetMaterial[targetMaterial]
+	for resource, cost := range robotCost.cost {
+		if status.resourceByMaterial[resource] < cost {
+			return false
+		}
+	}
+	return true
+}
+
+func (bp *Blueprint) AllNextStates(state RobotState) []RobotState {
+	possibleNextStates := []RobotState{}
+
+	if bp.CanBuild(state, geode) {
+		nextState := state.NextState()
+		robotCost := bp.robotCostByTargetMaterial[geode]
+		for material, cost := range robotCost.cost {
+			nextState.resourceByMaterial[material] -= cost
+		}
+		nextState.robotsByMaterial[geode] += 1
+		return []RobotState{nextState}
+	}
+
+	for _, targetMaterial := range MATERIALS {
+		if !bp.CanBuild(state, targetMaterial) {
+			continue
+		}
+		nextState := state.NextState()
+		robotCost := bp.robotCostByTargetMaterial[targetMaterial]
+		for material, cost := range robotCost.cost {
+			nextState.resourceByMaterial[material] -= cost
+		}
+		nextState.robotsByMaterial[targetMaterial] += 1
+		possibleNextStates = append(possibleNextStates, nextState)
+	}
+	// add no action status
+	possibleNextStates = append(possibleNextStates, state.NextState())
+	return possibleNextStates
+}
+
+const MAX_ITEMS = 10000000
+
+func (bp *Blueprint) FindMaxGeodes(maxTime int) int {
+	initState := RobotState{
+		time:               0,
+		resourceByMaterial: map[Material]int{},
+		robotsByMaterial:   map[Material]int{},
+	}
+	for _, material := range MATERIALS {
+		initState.resourceByMaterial[material] = 0
+		initState.robotsByMaterial[material] = 0
+	}
+	initState.robotsByMaterial[ore] = 1
+
+	processed := map[string]struct{}{initState.HashString(): {}}
+	states := []RobotState{initState}
+	for t := 0; t < maxTime; t++ {
+		oldStates := states
+		states = []RobotState{}
+		for _, state := range oldStates {
+			for _, nextState := range bp.AllNextStates(state) {
+				if _, found := processed[nextState.HashString()]; found {
+					continue
+				}
+				processed[nextState.HashString()] = struct{}{}
+				states = append(states, nextState)
+			}
+		}
+		sort.Slice(states, func(i, j int) bool {
+			return states[i].Score() > states[j].Score()
+		})
+		if len(states) > MAX_ITEMS {
+			states = states[0:MAX_ITEMS]
+		}
+	}
+	sort.Slice(states, func(i, j int) bool {
+		return states[i].resourceByMaterial[geode] > states[j].resourceByMaterial[geode]
+	})
+
+	return states[0].resourceByMaterial[geode]
 }
--- 2022/day_19/main_test.go
+++ 2022/day_19/main_test.go
@@ -6,12 +6,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const data = "Hello World !"
+const data = `Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.`
 
 func TestPart1(t *testing.T) {
-	assert.Equal(t, "Hello World !", part1(data), "Failed testing part 1")
+	assert.Equal(t, "33", part1(data), "Failed testing part 1")
 }
 
-func TestPart2(t *testing.T) {
-	assert.Equal(t, "Hello World !", part2(data), "Failed testing part 2")
+func TestFindMaxGeodes(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping part 2 tests")
+	}
+	bp1 := NewBlueprint("Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.")
+	assert.Equal(t, 9, bp1.FindMaxGeodes(24), "Failed testing blueprint 1 with 24 minutes")
+	assert.Equal(t, 56, bp1.FindMaxGeodes(32), "Failed testing blueprint 1 with 32 minutes")
+	bp2 := NewBlueprint("Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.")
+	assert.Equal(t, 12, bp2.FindMaxGeodes(24), "Failed testing blueprint 2 with 24 minutes")
+	assert.Equal(t, 62, bp2.FindMaxGeodes(32), "Failed testing blueprint 2 with 24 minutes")
 }
